7c9f98d6ad99a8b89306b5194752db90da20b5d8
Samatar Axmed <Samatar_267@hotmail.com>
2017-05-26 13:14:29 +0100

    TodoInputvalue is cleared on page render Changed timeline component to a stateless functional component and gave it a clear function. Relates to #71

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { TimelineList, TimelineItem, TimelineLink } from './base/Timeline';
 
-function Timeline({ steps }) {
-  return (
-    <TimelineList>
-      {steps.map(step => {
-        return (
-          <TimelineItem key={step.id}>
-            <TimelineLink
-              position={step.id % 2 === 0 ? 'right' : 'left'}
-              to={`/step-${step.id}`}
-            >
-              {step.title}
-            </TimelineLink>
-          </TimelineItem>
-        );
-      })}
-    </TimelineList>
-  );
+class Timeline extends Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    this.props.clearTodoInput();
+  }
+  render() {
+    return (
+      <TimelineList>
+        {this.props.steps.map(step => {
+          return (
+            <TimelineItem key={step.id}>
+              <TimelineLink
+                position={step.id % 2 === 0 ? 'right' : 'left'}
+                to={`/step-${step.id}`}
+              >
+                {step.title}
+              </TimelineLink>
+            </TimelineItem>
+          );
+        })}
+      </TimelineList>
+    );
+  }
 }
 
 export default Timeline;
